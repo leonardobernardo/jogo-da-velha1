f3da6cf08dce233e66de94863796259d3a82a380
leonardobernardo <leo-bully@hotmail.com>
2017-10-23 20:37:46 -0200

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a = new System.Windows.Forms.Button();
+            this.b = new System.Windows.Forms.Button();
+            this.c = new System.Windows.Forms.Button();
+            this.f = new System.Windows.Forms.Button();
+            this.d = new System.Windows.Forms.Button();
+            this.i = new System.Windows.Forms.Button();
+            this.h = new System.Windows.Forms.Button();
+            this.g = new System.Windows.Forms.Button();
+            this.btreset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // a
+            // 
+            this.a.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a.Location = new System.Drawing.Point(12, 96);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(75, 75);
+            this.a.TabIndex = 0;
+            this.a.UseVisualStyleBackColor = true;
+            this.a.Click += new System.EventHandler(this.a_Click);
+            // 
+            // b
+            // 
+            this.b.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.Location = new System.Drawing.Point(93, 96);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(75, 75);
+            this.b.TabIndex = 1;
+            this.b.UseVisualStyleBackColor = true;
+            this.b.Click += new System.EventHandler(this.b_Click);
+            // 
+            // c
+            // 
+            this.c.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c.Location = new System.Drawing.Point(174, 96);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(75, 75);
+            this.c.TabIndex = 2;
+            this.c.UseVisualStyleBackColor = true;
+            this.c.Click += new System.EventHandler(this.c_Click);
+            // 
+            // f
+            // 
+            this.f.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.f.Location = new System.Drawing.Point(174, 177);
+            this.f.Name = "f";
+            this.f.Size = new System.Drawing.Size(75, 75);
+            this.f.TabIndex = 5;
+            this.f.UseVisualStyleBackColor = true;
+            this.f.Click += new System.EventHandler(this.f_Click);
+            // 
+            // d
+            // 
+            this.d.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.d.Location = new System.Drawing.Point(12, 177);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(75, 75);
+            this.d.TabIndex = 3;
+            this.d.UseVisualStyleBackColor = true;
+            this.d.Click += new System.EventHandler(this.d_Click);
+            // 
+            // i
+            // 
+            this.i.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.i.Location = new System.Drawing.Point(174, 258);
+            this.i.Name = "i";
+            this.i.Size = new System.Drawing.Size(75, 75);
+            this.i.TabIndex = 8;
+            this.i.UseVisualStyleBackColor = true;
+            this.i.Click += new System.EventHandler(this.i_Click);
+            // 
+            // h
+            // 
+            this.h.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.h.Location = new System.Drawing.Point(93, 258);
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(75, 75);
+            this.h.TabIndex = 7;
+            this.h.UseVisualStyleBackColor = true;
+            this.h.Click += new System.EventHandler(this.h_Click);
+            // 
+            // g
+            // 
+            this.g.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.g.Location = new System.Drawing.Point(12, 258);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(75, 75);
+            this.g.TabIndex = 6;
+            this.g.UseVisualStyleBackColor = true;
+            this.g.Click += new System.EventHandler(this.g_Click);
+            // 
+            // btreset
+            // 
+            this.btreset.Location = new System.Drawing.Point(12, 361);
+            this.btreset.Name = "btreset";
+            this.btreset.Size = new System.Drawing.Size(237, 30);
+            this.btreset.TabIndex = 9;
+            this.btreset.Text = "Reset";
+            this.btreset.UseVisualStyleBackColor = true;
+            this.btreset.Click += new System.EventHandler(this.btreset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // eb
+            // 
+            this.eb.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eb.Location = new System.Drawing.Point(93, 177);
+            this.eb.Name = "eb";
+            this.eb.Size = new System.Drawing.Size(75, 75);
+            this.eb.TabIndex = 11;
+            this.eb.UseVisualStyleBackColor = true;
+            this.eb.Click += new System.EventHandler(this.eb_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 404);
+            this.Controls.Add(this.eb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btreset);
+            this.Controls.Add(this.i);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.f);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.a);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a;
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.Button c;
+        private System.Windows.Forms.Button f;
+        private System.Windows.Forms.Button d;
+        private System.Windows.Forms.Button i;
+        private System.Windows.Forms.Button h;
+        private System.Windows.Forms.Button g;
+        private System.Windows.Forms.Button btreset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button eb;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public string Jogador_Atual = "x";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string InverteJogador()
+        {
+            if (Jogador_Atual == "x")
+            {
+                return "o";
+            }
+            else
+            {
+                return "x";
+            }
+            return "x";
+        }
+        public void Zerar()
+        {
+            a.Text = "";
+            b.Text = "";
+            c.Text = "";
+            d.Text = "";
+            eb.Text = "";
+            f.Text = "";
+            g.Text = "";
+            h.Text = "";
+            i.Text = "";
+            Jogador_Atual = "x";
+        }
+        public void Ganhar()
+        {
+            MessageBox.Show("O jogador " + Jogador_Atual + " Ganhou!");
+            Zerar();
+        }
+        public bool verificar(string jogador_at)
+        { //VERTICAL
+            if (a.Text == jogador_at && d.Text == jogador_at && g.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            if (b.Text == jogador_at && eb.Text == jogador_at && h.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            if (c.Text == jogador_at && f.Text == jogador_at && i.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            //HORIZONTAL
+            if (a.Text == jogador_at && b.Text == jogador_at && c.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            if (d.Text == jogador_at && eb.Text == jogador_at && f.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            if (g.Text == jogador_at && h.Text == jogador_at && i.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            //DIAGONAL
+            if (a.Text == jogador_at && eb.Text == jogador_at && i.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+            if (c.Text == jogador_at && eb.Text == jogador_at && g.Text == jogador_at)
+            {
+                Ganhar();
+                return true;
+            }
+           
+            Jogador_Atual = InverteJogador();
+            label1.Text = "Jogador Atual: " + Jogador_Atual;
+            return false;
+
+        }
+
+
+
+        private void btreset_Click(object sender, EventArgs e)
+        {
+            Zerar();
+        }
+
+        private void a_Click(object sender, EventArgs e)
+        {
+            if (a.Text == "")
+            {
+                a.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            if (b.Text == "")
+            {
+                b.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            if (c.Text == "")
+            {
+                c.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void d_Click(object sender, EventArgs e)
+        {
+            if (d.Text == "")
+            {
+                d.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+       
+
+        private void f_Click(object sender, EventArgs e)
+        {
+            if (f.Text == "")
+            {
+                f.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void g_Click(object sender, EventArgs e)
+        {
+            if (g.Text == "")
+            {
+                g.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void h_Click(object sender, EventArgs e)
+        {
+            if (h.Text == "")
+            {
+                h.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void i_Click(object sender, EventArgs e)
+        {
+            if (i.Text == "")
+            {
+                i.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+
+        private void eb_Click_1(object sender, EventArgs e)
+        {
+            if (eb.Text == "")
+            {
+                eb.Text = Jogador_Atual;
+                verificar(Jogador_Atual);
+            }
+        }
+    }
+}
